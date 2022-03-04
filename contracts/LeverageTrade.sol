93fbb3df64c17e200ab69ff12ec3b4142125cae6
kifen <kifenraph@gmail.com>
2022-03-04 20:53:49 +0100

    Implement IterableMapping lib

--- contracts/LeverageTrade.sol
+++ contracts/LeverageTrade.sol
@@ -8,17 +8,28 @@ import {AMMExchange} from "./Exchange.sol";
 contract LeverageTrade is AMMExchange {
     uint256 public constant MAX_POS = 10;
 
-    struct Leverage {
+    struct Account {
+        uint256 totalCollateral;
         uint256 locked;
-        uint64 postion; // Pack struct into 2 slots.
+        uint64 postion; // Pack struct into 3 slots.
         address asset;
     }
 
+    struct Leverage {
+      uint256 collateral;
+      uint64 postion; // Pack struct into 3 slots.
+      address asset;
+    }
+
     mapping(address => Leverage[]) private positions;
+    mapping(IERC20 => bool) private tokens;
+    mapping(address => mapping(IERC20 => Leverage)) private positions;
+
 
     constructor(IERC20 twd, IERC20 usd) AMMExchange(twd, usd) {}
 
     function depositToken(IERC20 _token, uint256 _amount) external {
-
+        _safeTransferFrom(_token, msg.sender, address(this), _amount);
+        Account memory account = Account()
     }
 }
